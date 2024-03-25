81fc36bc55759833c4de622dc9631581fccb29f7
BlueJade <qq2441703573@163.com>
2024-03-25 18:44:37 +0800

    Create Datable in Editor,implements on apply a affect ,the affect will print on the screen

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,6 +3,7 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 void UOverlayWidgetController::BroadcastInitialValues()
@@ -32,6 +33,28 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute()).AddUObject(this, &UOverlayWidgetController::ManaChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
+	[this](const FGameplayTagContainer& AssetTags)
+	{
+		for(const auto Tag : AssetTags)
+		{
+			//For example, say that Tag = Message.HealthPotion
+			//"Message.HealthPotion".MatchesTag("Message") will return True, "Message".MatchesTag("Message.HealthPotion")wil return False
+
+			// const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+			// GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Green,Msg);
+			FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
+			if(Tag.MatchesTag(MessageTag))
+			{
+				const FUIWidgetRow* Row = GetDataTableByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+				MessageWidgetRowSignature.Broadcast(*Row);
+			}
+			
+			
+		}
+	}
+	);
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
